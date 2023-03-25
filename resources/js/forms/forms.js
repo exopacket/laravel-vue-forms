5e5a560a2483d0862576ac006df92c0d99089713
Ryan Fitzgerald <101024833+exopacket@users.noreply.github.com>
2023-03-25 13:25:47 -0400

    Update forms.js

--- resources/js/forms/forms.js
+++ resources/js/forms/forms.js
@@ -1,6 +1,4 @@
 export const forms = {
     LoginForm: await import("./login/login.js"),
     CreateAccountForm: await import("./create-account/create-account.js"),
-    CreateEventForm: await import("./events/create.js"),
-    CreateClassForm: await import("./create-class/create-class.js")
 }
